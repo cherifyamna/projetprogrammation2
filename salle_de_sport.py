080f8e20c575c22dc4310ef8568437decafbe5ed
amaradjida7-hub <amaradjida7@gmail.com>
2026-07-21 18:37:39 -0400

    file

---
+++ salle_de_sport.py
@@ -0,0 +1,11 @@
+class membre:
+    def __init__(self, numero, nom, succursale, duree. prix, actif):
+        self.numero = numero
+        slef.nom = nom
+        self.succursale = succursale
+        self.duree = duree
+        self.prix = prix
+        self.actif = actif
+    def afficher (self):
+        print(f"numero : {self.numero}") 
+        print(f"nom : {self.nom}")   
